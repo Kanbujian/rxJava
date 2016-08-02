d5657c626274508efa51bd0b91a378bacab994e6
462377353@qq.com <462377353@qq.com>
2016-08-02 21:26:54 +0800

    ddevelop update

---
+++ src/main/java/com/kanbujian/RxJavaRunner.java
@@ -0,0 +1,15 @@
+package com.kanbujian;
+
+/**
+ * Created by kanbujian on 16-8-1.
+ */
+public class RxJavaRunner {
+
+    /**
+     * 程序主入口
+     * @param args
+     */
+    public static void main(String[] args){
+
+    }
+}
